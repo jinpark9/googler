75c771e64ccb3d6a738f2063031acdfc115000fa
Jin Hyun Aron Park <jin@Jins-MacBook-Pro-2.local>
2020-07-13 00:57:48 -0700

    Create flag between lucky search and regualr search

--- src/Components/SearchResults.js
+++ src/Components/SearchResults.js
@@ -12,16 +12,34 @@ class SearchResults extends Component {
   constructor() {
     super();
     this.state = {
-      searchText:"ohlone land"
+      searchText:"ohlone land",
+      duckyText:"https://duckduckgo.com/?q=!ducky+",
+      regularText:"https://www.google.com/search?q=",
+      ducky:true,
+      searchEngine:""
     };
+  }
+
+  componentDidMount() {
+    if (this.state.ducky) {
+      this.setState({
+        searchEngine:this.state.duckyText
+      });
+    }
+    else { 
+      this.setState({
+        searchEngine:this.state.regularText
+      });
+    }
   }
 
   render() {
     return (
       <WebView
         source={{
-          // uri: `https://www.google.com/search?q=${this.state.searchText}&btnI`
-          uri: `https://duckduckgo.com/?q=!ducky+${this.state.searchText}`
+          // uri: `https://www.google.com/search?q=${this.state.searchText}`
+          // uri: `https://duckduckgo.com/?q=${this.state.duckyText}${this.state.searchText}`
+          uri: `${this.state.searchEngine}${this.state.searchText}`
         }}
         startInLoadingState={true}
         renderLoading={() => (
